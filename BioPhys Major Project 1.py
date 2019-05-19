c6df99cddeb8ea7ab6112ab2a141df437b9ba92c
semoyu <50421582+semoyu@users.noreply.github.com>
2019-05-19 11:10:53 -0700

    11:10 Changes

--- BioPhys Major Project 1.py
+++ BioPhys Major Project 1.py
@@ -17,69 +17,70 @@ N = np.loadtxt('/Users/ross/Desktop/Code/PH591/CenPop2010_OR.txt', skiprows=1, d
 lat = np.loadtxt('/Users/ross/Desktop/Code/PH591/CenPop2010_OR.txt', skiprows=1, delimiter=",", usecols=(5))
 long = np.loadtxt('/Users/ross/Desktop/Code/PH591/CenPop2010_OR.txt', skiprows=1, delimiter=",", usecols=(6))
 
-#Comm 1
-β = .9 #measels
-γ = (1/7) #recovery rate of measels
-α = .5 #1/r
-μ = (3.425*(10**-5)) #births per person per day
-p = .5
-
-'''Selected Parameters'''
-RefCity = 0
-PreInfected = 0
-RCPreInfected = 10
-EndTime = 3650 #Days
 
 μ = (11.8 / 1000)/365
-'''Tourism'''
-#Rate at which people visit
-α = []
-for i in range(0,len(N)):
-    dlon = (long[i] - long[RefCity])*np.pi/180
-    dlat = (lat[i] - lat[RefCity])*np.pi/180
-    a = (np.sin(dlat/2))**2 + np.cos(lat[RefCity]*np.pi/180) * np.cos(lat[i]*np.pi/180) * (np.sin(dlon/2))**2
-    c = 2 * np.arctan2( np.sqrt(a), np.sqrt(1-a) )
-    R = 6373 #Radius of Earth
-    d = R * c
-    if i == RefCity:
-        α.append(1)
-    else:
-        α.append(1/d)
-#Number of Infected in other Cities
-ICity = []
-for i in range(0,len(N)):
-    if i == RefCity:
-        ICity.append(RCPreInfected)
-    else:
-        ICity.append(PreInfected)
-#Number of Infected visiting the Reference City
-Sum = 0
-for i in range(0,len(N)):
-    Sum += α[i]*ICity[i]/N[i]
-#Creates arrays
-S = np.arange(0,EndTime)
-I = np.arange(0,EndTime)
-R = np.arange(0,EndTime)
-V = np.arange(0,EndTime)
-#Fills arrays over time
-for t in range(0,EndTime-1):
-    S[0] = N[RefCity] - ICity[RefCity]
-    I[0] = ICity[RefCity]
-    R[0] = 0
-    V[0] = 0
-    dS = (1-p)*μ*N[RefCity]-μ*S[t]-β*S[t]*(I[t]/N[RefCity] + Sum)
-    dI = β*S[t]*(I[t]/N[RefCity]) - γ*I[t] - μ*I[t]
-    dR = γ*I[t]-μ*R[t]
-    dV = p*μ*N[RefCity] - μ*V[t]
-    S[t+1] = S[t] + dS
-    I[t+1] = I[t] + dI
-    R[t+1] = R[t] + dR
-    V[t+1] = V[t] + dV
-plt.plot(np.arange(0,EndTime), S ,label='Susceptible')
-plt.plot(np.arange(0,EndTime), I ,label='Infected')
-plt.plot(np.arange(0,EndTime), R ,label='Recovered')
-plt.plot(np.arange(0,EndTime), V ,label='Vaccinated')
-plt.legend(loc ='best')
-plt.xlabel('Time (Days)')
-plt.ylabel('Population')
+def SIRV(β, γ, p, RefCity, RCPreInfected, PreInfected, Prevaccinated, EndTime):
+    α = []
+    for i in range(0,len(N)):
+        dlon = (long[i] - long[RefCity])*np.pi/180
+        dlat = (lat[i] - lat[RefCity])*np.pi/180
+        a = (np.sin(dlat/2))**2 + np.cos(lat[RefCity]*np.pi/180) * np.cos(lat[i]*np.pi/180) * (np.sin(dlon/2))**2
+        c = 2 * np.arctan2( np.sqrt(a), np.sqrt(1-a) )
+        R = 6373 #Radius of Earth
+        d = R * c
+        if i == RefCity:
+            α.append(1)
+        else:
+            α.append(1/d)
+    #Number of Infected in other Cities
+    ICity = []
+    for i in range(0,len(N)):
+        if i == RefCity:
+            ICity.append(RCPreInfected)
+        else:
+            ICity.append(PreInfected)
+    #Number of Infected visiting the Reference City
+    Sum = 0
+    for i in range(0,len(N)):
+        Sum += α[i]*ICity[i]/N[i]
+    #Creates arrays
+    S = np.arange(0,EndTime)
+    I = np.arange(0,EndTime)
+    R = np.arange(0,EndTime)
+    V = np.arange(0,EndTime)
+    #Fills arrays over time
+    for t in range(0,EndTime-1):
+        I[0] = ICity[RefCity]
+        R[0] = 0
+        V[0] = Prevaccinated
+        S[0] = N[RefCity] - ICity[RefCity] - R[0] - V[0]
+        dS = (1-p)*μ*N[RefCity]-μ*S[t]-β*S[t]*(I[t]/N[RefCity] + Sum)
+        dI = β*S[t]*(I[t]/N[RefCity]) - γ*I[t] - μ*I[t]
+        dR = γ*I[t]-μ*R[t]
+        dV = p*μ*N[RefCity] - μ*V[t]
+        S[t+1] = S[t] + dS
+        I[t+1] = I[t] + dI
+        R[t+1] = R[t] + dR
+        V[t+1] = V[t] + dV
+    plt.figure()
+    plt.plot(np.arange(0,EndTime), S ,label='Susceptible')
+    plt.plot(np.arange(0,EndTime), I ,label='Infected')
+    plt.plot(np.arange(0,EndTime), R ,label='Recovered')
+    plt.plot(np.arange(0,EndTime), V ,label='Vaccinated')
+    plt.plot(np.arange(0,EndTime), S+I+R+V ,label='Total Pop')
+    plt.legend(loc ='best')
+    plt.xlabel('Time (Days)')
+    plt.ylabel('Population')
+    plt.title('City: %i' %RefCity)
+
+'''SIRV(β, γ, p, RefCity, RCPreInfected, PreInfected, Prevaccinated, EndTime)'''
+SIRV(.4,1/100,.8,0,10,0,20,365)
+SIRV(.4,1/100,.8,0,10,0,0,365)
+
 plt.show()
+
+# '''Selected Parameters'''
+# RefCity = 0
+# PreInfected = 1
+# RCPreInfected = 10
+# EndTime = 100 #Days
